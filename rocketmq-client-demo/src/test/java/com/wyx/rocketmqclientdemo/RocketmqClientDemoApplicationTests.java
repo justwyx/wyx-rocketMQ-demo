7fdaa446ac5ecf44c1d9c8bd553529968a723869
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-10-20 16:37:06 +0800

    add

---
+++ rocketmq-client-demo/src/test/java/com/wyx/rocketmqclientdemo/RocketmqClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyx.rocketmqclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
